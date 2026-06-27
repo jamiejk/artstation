e164db283c837d57f9e335eeb3b4f1f8a9fdf64f
jamiejk <7187520+jamiejk@users.noreply.github.com>
2026-06-27 19:39:19 +0100

    Extend pen down servo range

--- axidraw_servo_conf.py
+++ axidraw_servo_conf.py
@@ -4,9 +4,14 @@
 #   servo_min = 9855  -> 0.82 ms, pen position 0
 #   servo_max = 27831 -> 2.32 ms, pen position 100
 #
-# The AxiDraw driver clamps pen_pos_up and pen_pos_down to 0-100. To get
-# more physical lift at pen_pos_up=100, increase servo_max in small steps
-# and test Pen Up after each change. Do not force the servo against a
-# mechanical stop.
-servo_min = 9855
+# The AxiDraw driver clamps pen_pos_up and pen_pos_down to 0-100.
+#
+# To get more physical downward travel at pen_pos_down=0, decrease servo_min
+# in small steps and test Pen Down after each change.
+#
+# To get more physical lift at pen_pos_up=100, increase servo_max in small
+# steps and test Pen Up after each change.
+#
+# Do not force the servo against a mechanical stop.
+servo_min = 9000
 servo_max = 28500
